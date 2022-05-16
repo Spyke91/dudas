8b5a778dea56dd86a4f7259460c3b084e3d7493a
Beatriz Borrego Polo <beatriz.borrego1@educa.madrid.org>
2022-05-16 14:35:10 +0200

    Simon Swipe

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+[
++SimonSwipeGame/nbproject/project.properties,f\5\f5e3a33e28f135a59863222ee3b06b05d3466f3e
+T
+$SimonSwipeGame/nbproject/project.xml,a\3\a30792db0e657bb04243a4d9d446fa67a28c436a
--- SimonSwipeGame/public_html/js/javascript.js
+++ SimonSwipeGame/public_html/js/javascript.js
@@ -1,53 +1,74 @@
-function startGame() {
-    document.getElementById("start").addEventListener('click', function () {
-        document.getElementById("rows").disabled = true;
-        document.getElementById("columns").disabled = true;
-        document.getElementById("level").disabled = true;
-        document.getElementById("message").innerHTML = "PLAYING...";
-        document.getElementById("crono").innerHTML += "0";
-        document.getElementById("score").innerHTML += "0";
-        const width = document.getElementById("columns").value;
-        const height = document.getElementById("rows").value;
+document.addEventListener('DOMContentLoaded', assignEvents);
 
-        drawField(width, height);
-        let cells = document.getElementsByName("cell");
-        enableEventsField(cells);
-        playGame(width, height);
-    });
+function assignEvents() {
+    document.getElementById("start").addEventListener('click', startGame);
+}
+
+function startGame() {
+    disableElements();
+    setInnerHTML();
+
+    const width = document.getElementById("columns").value;
+    const height = document.getElementById("rows").value;
+    drawField(width, height);
+
+    let cells = document.getElementsByName("cell");
+    enableEventsField(cells);
+
+    const range = document.getElementById("level").value;
+    const level = range * 5;
+
+    const gameSequence = generateSequence(width, height, level);
+    gameSequence.forEach(element => console.log(element));
+    playGame(cells, width, height, gameSequence);
+}
+
+function disableElements() {
+    document.getElementById("rows").disabled = true;
+    document.getElementById("columns").disabled = true;
+    document.getElementById("level").disabled = true;
+	document.getElementById("start").disabled = true;
+}
+
+function setInnerHTML() {
+    document.getElementById("message").innerHTML = "PLAYING...";
+    document.getElementById("crono").innerHTML += "0";
+    document.getElementById("score").innerHTML += "0";
 }
 
 function drawField(width, height) {
-    let colorRed = 255;
-    let colorBlue = 25;
-    let colorGreen = 10;
     for (i = 0; i < width; i++) {
         for (j = 0; j < height; j++) {
-            let field = document.getElementById("field");
-            let newCell = document.createElement("input");
-            field.appendChild(newCell);
-            newCell.id = i + "_" + j;
-            newCell.classList = "cell";
-            newCell.type = "button";
-            newCell.name = "cell";
-            let colorValue = "rgb(" + colorRed + ", " + colorGreen + ", " + colorBlue + ")";
-            newCell.style.backgroundColor = colorValue;
-            colorGreen += 10;
-            colorRed -= 10;
+            addCell(i, j);
         }
-        colorBlue += 55;
-        console.log(colorBlue);
-        let breakLine = document.createElement("br");
-        console.log(i);
-        console.log(j);
-        console.log(i + "_" + j);
-        console.log(document.getElementById(i + "_" + j));
-        let lastElement = document.getElementById(i + "_" + j);
-        lastElement.insertAdjacentElement('afterend', breakLine);
+        addLineBreak();
     }
 }
 
-function playGame(cells) {
-    disableEventsField(cells);
+function addCell(i, j) {
+    const colorRed = randomIntFromInterval(0, 255);
+    const colorBlue = randomIntFromInterval(0, 255);
+    const colorGreen = randomIntFromInterval(0, 255);
+    let field = document.getElementById("field");
+    let newCell = document.createElement("input");
+    field.appendChild(newCell);
+    newCell.id = i + "_" + j;
+    newCell.classList = "cellClass";
+    newCell.type = "button";
+    newCell.name = "cellName";
+    let colorValue = "rgb(" + colorRed + ", " + colorGreen + ", " + colorBlue + ")";
+    newCell.style.backgroundColor = colorValue;
+}
+
+function addLineBreak() {
+    let breakLine = document.createElement("br");
+    let length = document.getElementsByName("cellName").length;
+    let lastElement = document.getElementsByName("cellName")[length - 1];
+    lastElement.insertAdjacentElement('afterend', breakLine);
+}
+
+function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 function enableEventsField(cells) {
@@ -60,16 +81,56 @@ function check(event) {
     let coords = event.target.id.split('_');
     let x = coords[0];
     let y = coords[1];
-    document.getElementById("message").innerHTML = 
+    document.getElementById("message").innerHTML =
             "The button (" + x + "," + y + ") has been clicked.";
 }
 
-function disableEventsField(cells){
+function generateSequence(width, height, level) {
+    let sequence = [];
+    for (i = 0; i < level; i++) {
+        let min = 0;
+        sequence.push([randomIntFromInterval(min, width - 1)
+                    , randomIntFromInterval(min, height - 1)]);
+    }
+
+    return sequence;
+}
+
+function playGame(cells, width, height, sequence) {
+    let correct = true;
+
+    var roundNr = 0;
+    var i = setInterval(function () {
+        let x = sequence[roundNr][0];
+        let y = sequence[roundNr][1];
+        let elementId = x + "_" + y;
+        const previousColor = document.getElementById(elementId).style.backgroundColor;
+        document.getElementById(elementId).style.backgroundColor = "red";
+
+        document.getElementById(elementId).style.backgroundColor = previousColor;
+        roundNr++;
+
+    }, 1000);
+
+    if (roundNr === sequence.length) {
+        clearInterval(i);
+    }
+
+    //disableEventsField(cells);
+}
+
+function showElement(elementId) {
+    document.getElementById(elementId).style.backgroundColor = "red";
+}
+
+function disableEventsField(cells) {
     for (i = 0; i < cells.length; i++) {
         cells[i].removeEventListener('mousedown', check);
     }
 }
 
-document.addEventListener('DOMContentLoaded', startGame);
+
+
+
 
 
